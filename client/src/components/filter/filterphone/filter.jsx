995f15619ca7519ca1131fa521c0609324021417
Lucas <juangutierrezutnfrt@gmail.com>
2024-05-15 19:52:17 -0300

    cambios

--- client/src/components/filter/filterphone/filter.jsx
+++ client/src/components/filter/filterphone/filter.jsx
@@ -1,17 +1,29 @@
+"use client";
 import styles from "./filter.module.css"
+import { Select, Option } from "@material-tailwind/react";
 
 
 
-
-const FilterPhone = ({Marca}) => (
-    <div className={styles.container}>
-        <p>Filtrar</p>
-        <select>
-            <option>Goku phone</option> 
-        </select>
-        <select name={Marca}/>
-        <select name={Marca}/>
+const FilterPhone = () => (
+    // <div className={styles.container}>
+    //     <form>
+    //     <p>Filtrar</p>
+    //     <select>
+    //     <option>Goku phone</option> 
+    //     </select>
+    //     <select name={Marca}/>
+    //     <select name={Marca}/></form>
+    // </div>
+    <div className="w-72">
+      <Select label="Select Version">
+        <Option>Material Tailwind HTML</Option>
+        <Option>Material Tailwind React</Option>
+        <Option>Material Tailwind Vue</Option>
+        <Option>Material Tailwind Angular</Option>
+        <Option>Material Tailwind Svelte</Option>
+      </Select>
     </div>
+
 );
 
 
