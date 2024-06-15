e11840fc399bd83d2ac2b1f0521dea17e0170f02
Lucas <juangutierrezutnfrt@gmail.com>
2024-06-15 16:57:24 -0300

    detail,carousel

--- client/src/components/Carousel/carousel.jsx
+++ client/src/components/Carousel/carousel.jsx
@@ -11,19 +11,22 @@ export function CarouselDetail() {
         },
         {
             imagen:"https://samsungar.vtexassets.com/arquivos/ids/193747-800-auto?width=800&height=auto&aspect=true"
+        },
+        {
+            imagen:"https://samsungar.vtexassets.com/arquivos/ids/193753-800-auto?width=800&height=auto&aspect=true"
         }
     ]
 
     const [active, setActive] = React.useState("https://samsungar.vtexassets.com/arquivos/ids/193743-800-auto?width=800&height=auto&aspect=true");
     return(
         <div className="grid gap-4">
-            <div>
-                <img src={active} alt="" className="flex flex-wrap lg:w-1/2 w-full lg:h-auto h-auto object-cover object-center rounded" />
+            <div className="">
+                <img src={active} alt="" className="" />
             </div>
-            <div className="grid grid-cols-5 gap-4">
+            <div className=" flex ">
                 {data.map(({imagen}, index) => (
-                    <div key={index}>
-                        <img onClick={() => setActive(imagen)} src={imagen} alt="" className="h-20 cursor-pointer rounded-lg object-center" />
+                    <div key={index} className="">
+                        <img onClick={() => setActive(imagen)} src={imagen} alt="" className=" bg-white h-24 w-24 cursor-pointer rounded-lg object-cover object-center" />
                     </div>
                 ))}
             </div>
@@ -35,27 +38,3 @@ export function CarouselDetail() {
 
 
 
-//   return (
-//     <div className="grid gap-4">
-//       <div>
-//         <img
-//           className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
-//           src={active}
-//           alt=""
-//         />
-//       </div>
-//       <div className="grid grid-cols-5 gap-4">
-//         {data.map(({ imgelink }, index) => (
-//           <div key={index}>
-//             <img
-//               onClick={() => setActive(imgelink)}
-//               src={imgelink}
-//               className="h-20 max-w-full cursor-pointer rounded-lg object-cover object-center"
-//               alt="gallery-image"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
