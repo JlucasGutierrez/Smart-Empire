ac5ad5f303f42a06dba5cdba1edf5d485dac0852
Lucas <juangutierrezutnfrt@gmail.com>
2024-06-20 13:26:51 -0300

    tablet completada

---
+++ client/src/components/Card/viewphone.jsx
@@ -0,0 +1,6 @@
+
+export const ViewPhone = () =>{
+    return(
+        <div>view</div>
+    )
+}
--- client/src/components/Tablet/tabletPhone.jsx
+++ client/src/components/Tablet/tabletPhone.jsx
@@ -2,8 +2,8 @@
 
 export const TablePhone = () =>{
     return(
-        <div className="flex justify-center h-96">
-            <div className="grid justify-items-center bg-gray-100 w-3/4 ">
+        <div className="flex justify-center h-auto">
+            <div className="grid justify-items-center bg-gray-100 w-3/4 m-10 ">
                     <div>
                         <p className=" font-mono text-4xl italic uppercase m-10 ">Especificaciones técnicas</p>
                     </div>
@@ -24,8 +24,30 @@ export const TablePhone = () =>{
                         <p>256GB - 512GB</p>
                     </div>
                     <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
-
+                        <p>Tamaño de pantalla</p>
+                        <p>162.3 x 79.0 x 8.6mm</p>
                     </div>
+                    <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
+                        <p>Tipo de conectividad</p>
+                        <p> Wifi a/b/g/n/ac/ax/be 2.4GHz+5GHz+6GHz BT V5.3</p>
+                    </div>
+                    <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
+                        <p>SIM</p>
+                        <p>SIM + eSIM</p>
+                    </div>
+                    <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
+                        <p>NFC</p>
+                        <p>si</p>
+                    </div>
+                    <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
+                        <p>Bateria</p>
+                        <p>5,000mAh</p>
+                    </div>
+                    <div className=" border-t border-gray-400 py-2 w-3/4 flex justify-between">
+                        <p>Pin de carga</p>
+                        <p>USB Type C</p>
+                    </div>
+                    <div className="m-8"></div>
             </div>
         </div>
        
