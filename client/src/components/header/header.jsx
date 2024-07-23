60fb43b2fa732d66477e8a4f00713fbc2e92f9e5
Lucas <juangutierrezutnfrt@gmail.com>
2024-07-23 19:06:46 -0300

    login/modal

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,12 +1,10 @@
 "use client"
 import React from "react";
 import Link from "next/link"
+import { Login } from "../Login/login";
 
 function Header(){
-  const [openRight, setOpenRight] = React.useState(false);
-
-  const openDrawerRight = () => setOpenRight(true);
-  const closeDrawerRight = () => setOpenRight(false);
+ 
     return(
         <header class="bg-black bg-slate-900">
           <div className="flex justify-between w-full h-10 ">
@@ -19,14 +17,14 @@ function Header(){
               <button className="text-neutral-50 transition hover:text-purple-500/75 text-white dark:hover:text-white/75">Categorias</button>
             </div>
             <div className="flex justify-center mr-5 ">
-              <button 
+              <div><Login/></div>
+              {/* <button 
                className=" mr-3 group inline-block rounded bg-gradient-to-r from-purple-600 via-purple-500 to-purple-700 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
-               onClick={openDrawerRight}
               >
                  <span className="block rounded-sm bg-black px-8 py-2 text-sm font-medium text-white group-hover:bg-transparent">
                  Login
                 </span>
-              </button>
+              </button> */}
               <button className="group inline-block rounded bg-gradient-to-r from-purple-600 via-purple-500 to-purple-700 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75">
                 <span className="block rounded-sm bg-black px-8 py-2 text-sm font-medium text-white group-hover:bg-transparent">
                   Register
